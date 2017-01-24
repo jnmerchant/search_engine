42e7acca8dc0c7a20f24a28e1968ad5c0521562e
jnmerchant <joemercnc@gmail.com>
2017-01-24 14:50:58 -0500

    added data file

--- seed_search_engine.rb
+++ seed_search_engine.rb
@@ -1,3 +1,28 @@
-def seed_database()
-  
+#database name = search_engine
+#Amount Requested	Application Date	Loan Title	Risk_Score	Debt-To-Income Ratio	Zip Code	State	Employment Length	Policy Code
+require 'csv'
+conn = PG.connect(dbname: 'search_engine')
+file_path= '/Users/Joe/Documents/TIY/Week3/search_engine/data/q3_reject_stats.csv'
+
+begin
+  result = conn.exec('CREATE TABLE IF NOT EXISTS reject_stats (id serial primary key, amount numeric, application_date date, loan_title varchar,
+  risk_score integer, debt_to_income numeric, zip_code varchar, state varchar, employment_length varchar);')
+rescue PG::DuplicateTable => e
+  puts "That table already exists.."
+end
+
+def seed_database(conn)
+  CSV.foreach(file_path, {:headers => true }) do |row|
+    amount = row[0]
+    application_date = row[1]
+    loan_title = row[2]
+    risk_score = row[3]
+    debt_to_income = row[4]
+    zip_code = row[5]
+    state = row[6]
+    employment_length = row[7]
+
+    conn.exec("INSERT INTO reject_stats ;")
+  end
+
 end
